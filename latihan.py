7f7a1b41feb0eba3ea935550a3eff9c4acc46570
baehaqi <baehakisaputra72@gmail.com>
2021-11-01 12:17:47 +0700

    program penghitung luas dan keliling lingkaran

---
+++ latihan.py
@@ -0,0 +1,8 @@
+#program luas/keliling ligkaran
+#variable: luas, keliling, r, phi: float
+
+phi=22/7
+r=float(input("masukan jari-jari: "))
+luas=phi*r*r
+keliling = 2*phi*r
+print("hasil luas: %.2F"%luas,"keliling: %.2F"%keliling)
